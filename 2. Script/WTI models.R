784538807e70997d314f8f7247334475618d7479
paolamguio <ap.morales1967@uniandes.edu.co>
2022-08-02 22:49:16 -0500

    ajuste finales script

--- 2. Script/WTI models.R
+++ 2. Script/WTI models.R
@@ -1,5 +1,16 @@
+
+# Modelos univariables - WTI
+# Proyecto Final
+# Grupo 16
+# Andres Martinez, Paola Morales y Oscar Cortes 
+--------------------------------------------------
+  
+## preparación del espacio
 rm(list = ls())
 
+setwd("C:/Users/amorales/OneDrive - ANI/Documentos/GitHub/Proyecto_Final/4. Stores")
+
+## llamado librerías de la sesión
 require(pacman)
 p_load(
   tidyverse,
@@ -32,8 +43,11 @@ p_load(
   rugarch
 )
 
+###*** 1. Base de datos ***###
+## Llamado base de datos
 df <- read.xlsx("WTI.xlsx", 1)
 
+## ajustes a base de datos 
 df <- df %>%
   mutate(Month = yearmonth(Date)) %>%
   as_tsibble(index = Month)
@@ -42,11 +56,13 @@ df
 
 df %>% autoplot(WTI) + labs(title = "WTI Price", y = "WTI Price")
 
+# Creación valor retorno precio petróleo
 df <- df %>% mutate(lWTI = log(WTI))
 df <- df %>% mutate(rWTI = difference(lWTI))
 
 df %>% autoplot(rWTI) + labs(title = "WTI Price Returns", y = "WTI Price Returns")
 
+# análisis cuantílico de outliers, se imputan valores
 impute_outliers <- function(x, removeNA = TRUE){
   quantiles <- quantile(x, c(0.025, 0.975), na.rm = removeNA)
   x[x<quantiles[1]] <- mean(x, na.rm = removeNA)
@@ -58,12 +74,15 @@ df <- df %>% mutate(rWTI_A = impute_outliers(rWTI))
 
 df %>% autoplot(rWTI_A) + labs(title = "WTI Price Returns Outliers Adjusted", y = "WTI Price Returns Outliers Adjusted")
 
+# se definen bases de datos de train y test
 df_train <- df %>% filter_index(. ~ "2014 Oct")
 df_test <- df %>% filter_index("2014 Sep" ~ "2020 Dic")
 
 df_train %>%
   gg_tsdisplay(rWTI_A, plot_type='partial')
 
+###*** 2. Creación de modelos ***###
+## 2.1. Modelo ARIMA
 rWTI_fit <- df_train %>%
   model(arima100 = ARIMA(rWTI_A ~ pdq(1,0,0)),
         arima001 = ARIMA(rWTI_A ~ pdq(0,0,1)),
@@ -96,6 +115,7 @@ checkresiduals(err_pron1[!is.na(err_pron1)])
 
 Box.test(err_pron1[!is.na(err_pron1)], type = "Ljung-Box")
 
+# se genera pronóstico un paso adelante
 pron_1<-rWTI_ts-err_pron1
 
 plot(pron_1)
@@ -109,6 +129,7 @@ df_train$residuals2 <- df_train$residuals^2
 df_train %>%
   gg_tsdisplay(residuals2, plot_type='partial')
 
+## 2.2. Modelo Garch
 rWTI_train=ts(df_train$rWTI_A,
          start = c(1989, 1), frequency = 12)
 
@@ -146,6 +167,7 @@ Box.test(err_pron2[!is.na(err_pron2)], type = "Ljung-Box")
 lm_m2<-lm(rWTI_ts~f1, na.action = na.omit)
 summary(lm_m2)
 
+## 2.3. Modelo T-Garch
 spec2 <- ugarchspec(variance.model = list(model = "fGARCH", submodel = "TGARCH", garchOrder = c(1, 1)),
                    mean.model = list(armaOrder = c(1, 0),
                                      include.mean = FALSE),
@@ -177,6 +199,7 @@ Box.test(err_pron3[!is.na(err_pron3)], type = "Ljung-Box")
 lm_m3<-lm(rWTI_ts~f2, na.action = na.omit)
 summary(lm_m3)
 
+###*** 3. Accuracy modelos ***###
 accuracyARIMA <- accuracy(pron_1, rWTI_ts)
 rownames(accuracyARIMA) <- "ARIMA Model"
 
@@ -193,6 +216,7 @@ accuracy <- as.data.frame(accuracy)
 accuracy$Model <- row.names(accuracy)
 accuracy <- accuracy %>% select(Model, everything())
 
+# generación base de datos
 write_xlsx(accuracy, "WTI Models.xlsx")
 
 WTI_proyecciones <- cbind(f1, f2)
--- 2. Script/WTI models2.R
+++ 2. Script/WTI models2.R
@@ -1,5 +1,16 @@
+
+# Modelos multivariados - WTI
+# Proyecto Final
+# Grupo 16
+# Andres Martinez, Paola Morales y Oscar Cortes 
+--------------------------------------------------
+  
+## preparación del espacio
 rm(list = ls())
 
+setwd("C:/Users/amorales/OneDrive - ANI/Documentos/GitHub/Proyecto_Final/4. Stores")
+
+## llamado librerías de la sesión
 require(pacman)
 p_load(
   tidyverse,
@@ -34,6 +45,8 @@ p_load(
   xgboost
 )
 
+###*** 1. Base de datos ***###
+## Llamado base de datos
 df <- import("df.rds")
 
 df$month <- as.factor(month(df$Date))
@@ -80,6 +93,7 @@ df <- df[,c(3, 5:31)]
 
 df3 <- df[7:400,]
 
+# se crea base de datos test
 df_test <- df[310:383,]
 
 df_forecast <- df[384:400,]
@@ -106,9 +120,13 @@ df_forecast2 <- df2[205:221,]
 
 df2 <- df2[7:204,]
 
+###*** 2. Creación de modelos ***###
+
+## Serie WTI mensual de enero de 1989 a mayo de 2022
 rWTI_ts<-ts(df$rWTI_A, start = c(1989, 8), frequency = 12)
 rWTI_ts<-window(rWTI_ts, start=c(2014, 11))
 
+## Serie WTI mensual + variables gas natural + variables google trend
 rWTI_ts2<-ts(df2$rWTI_A, start = c(2004, 7), frequency = 12)
 rWTI_ts2<-window(rWTI_ts2, start=c(2017, 9))
 
@@ -119,6 +137,7 @@ control <- trainControl(method = "timeslice",
                               horizon = 1,
                               fixedWindow = FALSE)
 
+# 2.1 Elastic Net
 elasticnet1 <- train(
   rWTI_A~.,
   data = df,
@@ -148,6 +167,8 @@ elasticnet2 <- train(
 
 predElasticnet2<-predict(elasticnet2,df_test2)
 
+# 2.2. Booststrap
+# se define grilla
 grid_default <- expand.grid(nrounds = c(100,250),
                             max_depth = c(4,6,8),
                             eta = c(0.01,0.3,0.5),
@@ -285,6 +306,7 @@ predpca2<-ts(predpca2, start = c(2017, 9), frequency = 12)
 predxgboost1<-ts(predxgboost1, start = c(2014, 11), frequency = 12)
 predxgboost2<-ts(predxgboost2, start = c(2017, 9), frequency = 12)
 
+###*** 3. Accuracy modelos ***###
 accuracyElasticnet <- accuracy(predElasticnet1, rWTI_ts)
 rownames(accuracyElasticnet) <- "Elastic Net Model 1"
 
@@ -319,6 +341,7 @@ accuracy <- as.data.frame(accuracy)
 accuracy$Model <- row.names(accuracy)
 accuracy <- accuracy %>% select(Model, everything())
 
+# generación base de datos
 write_xlsx(accuracy, "WTI Models2.xlsx")
 
 plot(predpca2)
@@ -338,6 +361,7 @@ WTI_proyecciones <- ts.union(rWTI_A = rWTI_ts, WTI_proyecciones, WTI_proyeccione
 
 WTI_proyecciones %>% autoplot()
 
+# generación base de datos
 WTI_proyecciones3 <- import("WTI proyeccion.rds")
 
 WTI_proyecciones <- ts.union(WTI_proyecciones, WTI_proyecciones3)
