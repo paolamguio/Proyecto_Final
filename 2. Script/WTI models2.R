a3546db0a5f74ea1e3701918f73c15d76e0be3b1
felipemartinez0812 <af.martinezv12@uniandes.edu.co>
2022-08-02 19:30:44 -0500

    Se actualiza script

--- 2. Script/WTI models2.R
+++ 2. Script/WTI models2.R
@@ -1,16 +1,4 @@
-
-# Modelos Multivariables - WTI
-# Proyecto Final
-# Grupo 16
-# Andres Martinez, Paola Morales y Oscar Cortes 
-#--------------------------------------------------
-  
-## preparación del espacio
 rm(list = ls())
-
-setwd("C:/Users/amorales/OneDrive - ANI/Documentos/GitHub/Proyecto_Final/4. Stores")
-setwd("C:/Users/ocaco/OneDrive/15. Maestria Economia/9. Big Data/3. GitHub/Proyecto_Final/4. Stores")
-
 
 require(pacman)
 p_load(
@@ -46,14 +34,14 @@ p_load(
   xgboost
 )
 
-###*** 1. Base de datos ***###
-## Llamado base de datos
 df <- import("df.rds")
 
-## ajustes a base de datos 
 df$month <- as.factor(month(df$Date))
 
-# se crea variable de logaritmo del WTI
+rWTI_A <- df$rWTI_A
+
+rWTI_A<-ts(rWTI_A, start = c(1989, 1), frequency = 12)
+
 function1 <- function(x){
   log <- log(x)
   colnames(log) <- paste("log", colnames(x))
@@ -90,10 +78,13 @@ df <- bind_cols(df, lag_df)
 
 df <- df[,c(3, 5:31)]
 
-# se crea base de datos test
-df_test <- df[320:400,]
+df3 <- df[7:400,]
 
-df <- df[7:400,]
+df_test <- df[310:383,]
+
+df_forecast <- df[384:400,]
+
+df <- df[7:383,]
 
 lag_df2 <- NULL
 
@@ -107,27 +98,27 @@ df2 <- bind_cols(df2, lag_df2)
 
 df2 <- df2[,c(2:22, 24, 26:178)]
 
-df_test2 <- df2[176:221,]
+df4 <- df2[7:221,]
 
-df2 <- df2[7:221,]
+df_test2 <- df2[165:204,]
 
-###*** 2. Creación de modelos ***###
+df_forecast2 <- df2[205:221,]
 
-## Serie WTI mensual de enero de 1989 a mayo de 2022
-rWTI_ts<-ts(df$rWTI_A, start = c(1989, 8), frequency = 12) 
-rWTI_ts<-window(rWTI_ts, start=c(2015, 9))
+df2 <- df2[7:204,]
 
-## Serie WTI mensual + variables macro de USA + variables google trend
+rWTI_ts<-ts(df$rWTI_A, start = c(1989, 8), frequency = 12)
+rWTI_ts<-window(rWTI_ts, start=c(2014, 11))
+
 rWTI_ts2<-ts(df2$rWTI_A, start = c(2004, 7), frequency = 12)
-rWTI_ts2<-window(rWTI_ts2, start=c(2018, 8))
+rWTI_ts2<-window(rWTI_ts2, start=c(2017, 9))
 
 set.seed(1)
 
 control <- trainControl(method = "timeslice",
-                              initialWindow = 313,
+                              initialWindow = 303,
                               horizon = 1,
                               fixedWindow = FALSE)
-# 2.1 Elastic Net
+
 elasticnet1 <- train(
   rWTI_A~.,
   data = df,
@@ -142,7 +133,7 @@ predElasticnet1<-predict(elasticnet1,df_test)
 set.seed(1)
 
 control2 <- trainControl(method = "timeslice",
-                        initialWindow = 169,
+                        initialWindow = 158,
                         horizon = 1,
                         fixedWindow = FALSE)
 
@@ -157,8 +148,6 @@ elasticnet2 <- train(
 
 predElasticnet2<-predict(elasticnet2,df_test2)
 
-# 2.2. Boost
-# se define grilla
 grid_default <- expand.grid(nrounds = c(100,250),
                             max_depth = c(4,6,8),
                             eta = c(0.01,0.3,0.5),
@@ -193,31 +182,35 @@ xgboost2 <- train(
   preProcess = c("center", "scale")
 )
 
-predxgboost2<-predict(xgboost2,df_testing2)
+predxgboost2<-predict(xgboost2,df_test2)
 
-pca <- prcomp(df[,2:28], scale = TRUE)
+pca <- prcomp(df3[,2:28], scale = TRUE)
 
 pca <- pca$x
 
 pca <- as.data.frame(pca)
 
-pca <- pca[,1:4]
+pca_forecast <- pca[378:394,1:4]
+
+pca <- pca[1:377,1:4]
 
 pca <- bind_cols(rWTI_A = df$rWTI_A, pca)
 
-pca_test <- pca[314:394,]
+pca_test <- pca[304:377,]
 
-pca2 <- prcomp(df2[,c(1:21, 23:175)], scale = TRUE)
+pca2 <- prcomp(df4[,c(1:21, 23:175)], scale = TRUE)
 
 pca2 <- pca2$x
 
 pca2 <- as.data.frame(pca2)
 
-pca2 <- pca2[,1:4]
+pca_forecast2 <- pca2[199:215,1:4]
+
+pca2 <- pca2[1:198,1:4]
 
 pca2 <- bind_cols(rWTI_A = df2$rWTI_A, pca2)
 
-pca_test2 <- pca[170:215,]
+pca_test2 <- pca[159:198,]
 
 pcamodel <- train(
   rWTI_A~.,
@@ -240,20 +233,21 @@ pcamodel2 <- train(
 predpca2 <- predict(pcamodel2 , pca_test2)
 
 rWTI_ts<-ts(df$rWTI_A, start = c(1989, 8), frequency = 12)
-rWTI_train<-window(rWTI_ts, end=c(2015, 8))
-rWTI_ts<-window(rWTI_ts, start=c(2015, 9))
+rWTI_train<-window(rWTI_ts, end=c(2014, 10))
+rWTI<-window(rWTI_ts, end=c(2020, 12))
+rWTI_ts<-window(rWTI_ts, start=c(2014, 11))
 
 rWTI_ts2<-ts(df2$rWTI_A, start = c(2004, 7), frequency = 12)
-rWTI_train2<-window(rWTI_ts2, end=c(2018, 7))
-rWTI_ts2<-window(rWTI_ts2, start=c(2018, 8))
+rWTI_train2<-window(rWTI_ts2, end=c(2017, 8))
+rWTI_ts2<-window(rWTI_ts2, start=c(2017, 9))
 
 df_ts<-ts(df, start = c(1989, 8), frequency = 12)
-x_train<-window(df_ts[,2:28], end=c(2015, 8))
-x_test<-window(df_ts[,2:28], start=c(2015, 9))
+x_train<-window(df_ts[,2:28], end=c(2014, 10))
+x_test<-window(df_ts[,2:28], start=c(2014, 11))
 
 df_ts2<-ts(df2, start = c(2004, 7), frequency = 12)
-x_train2<-window(df_ts2[,c(1:21, 23:175)], end=c(2018, 7))
-x_test2<-window(df_ts2[,c(1:21, 23:175)], start=c(2018, 8))
+x_train2<-window(df_ts2[,c(1:21, 23:175)], end=c(2017, 8))
+x_test2<-window(df_ts2[,c(1:21, 23:175)], start=c(2017, 9))
 
 fourier_xreg<-fourier(rWTI_train, K = 5)
 
@@ -262,7 +256,7 @@ set.seed(1)
 nnet<-nnetar(rWTI_train, repeats = 150, p = 1, 
                      P = 1, size = 3, xreg = fourier_xreg)
 
-prednnet<-forecast(nnet, h=81, xreg = fourier(rWTI_train, K = 5, h = 81))
+prednnet<-forecast(nnet, h=74, xreg = fourier(rWTI_train, K = 5, h = 81))
 
 prednnet <- prednnet$mean
 
@@ -271,7 +265,7 @@ set.seed(1)
 nnet2<-nnetar(rWTI_train, repeats = 150, p = 1, 
              P = 1, size = 3, xreg = x_train)
 
-prednnet2<-forecast(nnet2, h=81, xreg = x_test)
+prednnet2<-forecast(nnet2, h=74, xreg = x_test)
 
 prednnet2 <- prednnet2$mean
 
@@ -280,18 +274,17 @@ set.seed(1)
 nnet3<-nnetar(rWTI_train2, repeats = 150, p = 1, 
               P = 1, size = 3, xreg = x_train2)
 
-prednnet3<-forecast(nnet3, h=46, xreg = x_test2)
+prednnet3<-forecast(nnet3, h=40, xreg = x_test2)
 
 prednnet3 <- prednnet3$mean
 
-predElasticnet1<-ts(predElasticnet1, start = c(2015, 9), frequency = 12)
-predElasticnet2<-ts(predElasticnet2, start = c(2018, 8), frequency = 12)
-predpca<-ts(predpca, start = c(2015, 9), frequency = 12)
-predpca2<-ts(predpca2, start = c(2018, 8), frequency = 12)
-predxgboost1<-ts(predxgboost1, start = c(2015, 9), frequency = 12)
-predxgboost2<-ts(predxgboost2, start = c(2018, 8), frequency = 12)
+predElasticnet1<-ts(predElasticnet1, start = c(2014, 11), frequency = 12)
+predElasticnet2<-ts(predElasticnet2, start = c(2017, 9), frequency = 12)
+predpca<-ts(predpca, start = c(2014, 11), frequency = 12)
+predpca2<-ts(predpca2, start = c(2017, 9), frequency = 12)
+predxgboost1<-ts(predxgboost1, start = c(2014, 11), frequency = 12)
+predxgboost2<-ts(predxgboost2, start = c(2017, 9), frequency = 12)
 
-###*** 3. Accuracy modelos ***###
 accuracyElasticnet <- accuracy(predElasticnet1, rWTI_ts)
 rownames(accuracyElasticnet) <- "Elastic Net Model 1"
 
@@ -326,5 +319,66 @@ accuracy <- as.data.frame(accuracy)
 accuracy$Model <- row.names(accuracy)
 accuracy <- accuracy %>% select(Model, everything())
 
-# generación base de datos
 write_xlsx(accuracy, "WTI Models2.xlsx")
+
+plot(predpca2)
+plot(predElasticnet2)
+plot(rWTI_ts2)
+
+WTI_proyecciones <- cbind(predpca, predElasticnet1)
+
+WTI_proyecciones2 <- cbind(predpca2, predElasticnet2)
+
+plot(WTI_proyecciones)
+
+rWTI_ts<-ts(df$rWTI_A, start = c(1989, 8), frequency = 12)
+rWTI_ts<-window(rWTI_ts, start=c(2012, 1))
+
+WTI_proyecciones <- ts.union(rWTI_A = rWTI_ts, WTI_proyecciones, WTI_proyecciones2)
+
+WTI_proyecciones %>% autoplot()
+
+WTI_proyecciones3 <- import("WTI proyeccion.rds")
+
+WTI_proyecciones <- ts.union(WTI_proyecciones, WTI_proyecciones3)
+
+saveRDS(WTI_proyecciones, file = "WTI proyecciones.rds")
+
+WTI_proyecciones %>% autoplot()
+
+rWTI_Elasticnet_forecast<-predict(elasticnet2,df_forecast2)
+
+rWTI_Elasticnet_forecast=ts(rWTI_Elasticnet_forecast,
+                       start = c(2021, 1), frequency = 12)
+
+plot(rWTI_Elasticnet_forecast)
+
+rWTI_PCA_forecast <- predict(pcamodel2 , pca_forecast2)
+
+rWTI_PCA_forecast=ts(rWTI_PCA_forecast,
+                            start = c(2021, 1), frequency = 12)
+
+plot(rWTI_PCA_forecast)
+
+fourier_xreg<-fourier(rWTI, K = 5)
+
+set.seed(1)
+
+nnet<-nnetar(rWTI, repeats = 150, p = 1, 
+             P = 1, size = 3, xreg = fourier_xreg)
+
+rWTI_NNET_forecast<-forecast(nnet, h=17, xreg = fourier(rWTI, K = 5, h = 17))
+
+rWTI_NNET_forecast <- rWTI_NNET_forecast$mean
+
+plot(rWTI_NNET_forecast)
+
+rWTI_A<-window(rWTI_A, start=c(2018, 1))
+
+WTI_forecast <- ts.union(rWTI_A = rWTI_A, rWTI_Elasticnet_forecast, rWTI_PCA_forecast, rWTI_NNET_forecast)
+
+WTI_forecast[36,2] <- WTI_forecast[36,1]
+WTI_forecast[36,3] <- WTI_forecast[36,1]
+WTI_forecast[36,4] <- WTI_forecast[36,1]
+
+WTI_forecast %>% autoplot()
