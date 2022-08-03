07f388da9826c061ddcfb7f3ba1d28f7258be141
paolamguio <ap.morales1967@uniandes.edu.co>
2022-08-02 22:50:37 -0500

    Ajuste nombre script

--- 2. Script/2_WTI models.R
+++ 2. Script/2_WTI models.R
@@ -46,7 +46,6 @@ p_load(
 ###*** 1. Base de datos ***###
 ## Llamado base de datos
 df <- read.xlsx("WTI.xlsx", 1)
-df <- WTI
 
 ## ajustes a base de datos 
 df <- df %>%
@@ -57,7 +56,7 @@ df
 
 df %>% autoplot(WTI) + labs(title = "WTI Price", y = "WTI Price")
 
-# Creación variables WTI
+# Creación valor retorno precio petróleo
 df <- df %>% mutate(lWTI = log(WTI))
 df <- df %>% mutate(rWTI = difference(lWTI))
 
@@ -76,8 +75,8 @@ df <- df %>% mutate(rWTI_A = impute_outliers(rWTI))
 df %>% autoplot(rWTI_A) + labs(title = "WTI Price Returns Outliers Adjusted", y = "WTI Price Returns Outliers Adjusted")
 
 # se definen bases de datos de train y test
-df_train <- df %>% filter_index(. ~ "2015 Aug")
-df_test <- df %>% filter_index("2015 Sep" ~ .)
+df_train <- df %>% filter_index(. ~ "2014 Oct")
+df_test <- df %>% filter_index("2014 Sep" ~ "2020 Dic")
 
 df_train %>%
   gg_tsdisplay(rWTI_A, plot_type='partial')
@@ -94,17 +93,17 @@ rWTI_fit <- df_train %>%
 glance(rWTI_fit) %>% arrange(AICc) %>% select(.model:BIC)
 
 rWTI_fit %>%
-  select(arima100) %>%
+  select(search) %>%
   gg_tsresiduals()
 
 augment(rWTI_fit) %>%
-  filter(.model=='arima100') %>%
+  filter(.model=='search') %>%
   features(.innov, ljung_box, lag = 10, dof = 3)
 
 rWTI_arima <- arima(df_train$rWTI_A,c(1,0,0))
 
-rWTI_ts<-ts(df$rWTI_A, start = c(1989, 1), frequency = 12)
-rWTI_ts<-window(rWTI_ts, start=c(2015, 9))
+rWTI_ts<-ts(df[1:384,]$rWTI_A, start = c(1989, 1), frequency = 12)
+rWTI_ts<-window(rWTI_ts, start=c(2014, 11))
 
 fun_m1<-function(x, h=h){
   forecast(Arima(x,order=c(1,0,0), include.drift = T), h=h)
@@ -116,7 +115,7 @@ checkresiduals(err_pron1[!is.na(err_pron1)])
 
 Box.test(err_pron1[!is.na(err_pron1)], type = "Ljung-Box")
 
-# se genera pronóstico de un paso adelante
+# se genera pronóstico un paso adelante
 pron_1<-rWTI_ts-err_pron1
 
 plot(pron_1)
@@ -131,11 +130,10 @@ df_train %>%
   gg_tsdisplay(residuals2, plot_type='partial')
 
 ## 2.2. Modelo Garch
-
 rWTI_train=ts(df_train$rWTI_A,
          start = c(1989, 1), frequency = 12)
 
-rWTI=ts(df$rWTI_A,
+rWTI=ts(df[1:384,]$rWTI_A,
               start = c(1989, 1), frequency = 12)
 
 spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
@@ -144,13 +142,13 @@ spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1))
                    distribution.model = "norm")
 
 rWTI_garch<-ugarchfit(spec=spec,
-                   data=rWTI_train[2:320])
+                   data=rWTI_train[2:310])
 
 f1 = NULL
 
-for (i in 0:81) {
+for (i in 0:74) {
   rWTI_garch<-ugarchfit(spec=spec,
-                        data=rWTI[2:(320+i)])
+                        data=rWTI[2:(310+i)])
   rWTI_garch_forecast <- ugarchforecast(rWTI_garch, n.ahead = 1)
   f1[i] <- rWTI_garch_forecast@forecast$seriesFor[1]
 }
@@ -170,20 +168,19 @@ lm_m2<-lm(rWTI_ts~f1, na.action = na.omit)
 summary(lm_m2)
 
 ## 2.3. Modelo T-Garch
-
 spec2 <- ugarchspec(variance.model = list(model = "fGARCH", submodel = "TGARCH", garchOrder = c(1, 1)),
                    mean.model = list(armaOrder = c(1, 0),
                                      include.mean = FALSE),
                    distribution.model = "norm")
 
 rWTI_tgarch<-ugarchfit(spec=spec2,
-                      data=rWTI_train[2:320])
+                      data=rWTI_train[2:310])
 
 f2 = NULL
 
-for (i in 0:81) {
+for (i in 0:74) {
   rWTI_tgarch<-ugarchfit(spec=spec2,
-                        data=rWTI[2:(320+i)])
+                        data=rWTI[2:(310+i)])
   rWTI_tgarch_forecast <- ugarchforecast(rWTI_tgarch, n.ahead = 1)
   f2[i] <- rWTI_tgarch_forecast@forecast$seriesFor[1]
 }
@@ -203,7 +200,6 @@ lm_m3<-lm(rWTI_ts~f2, na.action = na.omit)
 summary(lm_m3)
 
 ###*** 3. Accuracy modelos ***###
-
 accuracyARIMA <- accuracy(pron_1, rWTI_ts)
 rownames(accuracyARIMA) <- "ARIMA Model"
 
@@ -223,4 +219,20 @@ accuracy <- accuracy %>% select(Model, everything())
 # generación base de datos
 write_xlsx(accuracy, "WTI Models.xlsx")
 
+WTI_proyecciones <- cbind(f1, f2)
+
+saveRDS(WTI_proyecciones, file = "WTI proyeccion.rds")
+
 saveRDS(df, file = "df.rds")
+
+rWTI_garch<-ugarchfit(spec=spec,
+                      data=rWTI[2:384])
+
+rWTI_garch_forecast <- ugarchforecast(rWTI_garch, n.ahead = 17)
+
+rWTI_garch_forecast <- rWTI_garch_forecast@forecast$seriesFor
+
+rWTI_garch_forecast=ts(rWTI_garch_forecast,
+      start = c(2021, 1), frequency = 12)
+
+plot(rWTI_garch_forecast)
